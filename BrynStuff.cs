e55b095a75c7875bbd5ef6b47c354a621ae65aa9
BrynDino <47651123+ishan-chandran@users.noreply.github.com>
2020-07-06 15:06:07 -0700

    Add files via upload

---
+++ BrynStuff.cs
@@ -0,0 +1,25 @@
+using Ionic.Zip;
+using System;
+using System.Security.AccessControl;
+using Terraria.ModLoader;
+
+namespace BrynStuff
+{
+	public class BrynStuff : Mod
+	{
+		public BrynStuff()
+		{
+		}
+		
+		class MPStuff : ModPlayer
+        {
+			public ModPlayer modPlayer;
+			public bool ratSummonProjectile;
+
+		}
+
+        internal class Tiles
+        {
+        }
+    }
+}
